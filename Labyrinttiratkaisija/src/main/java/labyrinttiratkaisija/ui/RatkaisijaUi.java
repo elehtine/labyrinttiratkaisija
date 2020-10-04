f3c4e7ce015dbea5456e89e2ee9989b6ad5a10f4
Elias Lehtinen <elias.lehtinen01@gmail.com>
2020-10-04 14:14:49 +0300

    User interface can change showed maze with generated new one

--- Labyrinttiratkaisija/src/main/java/labyrinttiratkaisija/ui/RatkaisijaUi.java
+++ Labyrinttiratkaisija/src/main/java/labyrinttiratkaisija/ui/RatkaisijaUi.java
@@ -3,7 +3,10 @@ package labyrinttiratkaisija.ui;
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
+import javafx.scene.control.Button;
 import javafx.scene.layout.GridPane;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
@@ -24,10 +27,41 @@ public class RatkaisijaUi extends Application {
      */
     @Override
     public void start(Stage ikkuna) {
-        GridPane ruudukko = new GridPane();
+        Button kruskal = new Button("Kruskalin algoritmi");
+        Button syvyyshaku = new Button("Svyyshaku");
+        Button takaisinLuomisesta = new Button("Takaisin");
+        VBox luontiNapit = new VBox();
+        luontiNapit.getChildren().add(new Label("Valitse algoritmi"));
+        luontiNapit.getChildren().add(kruskal);
+        luontiNapit.getChildren().add(syvyyshaku);
+        luontiNapit.getChildren().add(takaisinLuomisesta);
+
+        Scene luontiNakyma = new Scene(luontiNapit);
 
         char[][] kartta = LuontiKruskalilla.luo(19, 19);
         Labyrintti labyrintti = new Labyrintti(kartta);
+        Scene labyrinttiNakyma = getLabyrinttiNakyma(labyrintti, luontiNakyma, ikkuna);
+
+        kruskal.setOnAction((action) -> {
+            Labyrintti l = new Labyrintti(LuontiKruskalilla.luo(19, 19));
+            ikkuna.setScene(getLabyrinttiNakyma(l, luontiNakyma, ikkuna));
+        });
+
+        syvyyshaku.setOnAction((action) -> {
+            Labyrintti l = new Labyrintti(LuontiSyvyyshaulla.luo(19, 19));
+            ikkuna.setScene(getLabyrinttiNakyma(l, luontiNakyma, ikkuna));
+        });
+
+        takaisinLuomisesta.setOnAction((action) -> {
+            ikkuna.setScene(labyrinttiNakyma);
+        });
+
+        ikkuna.setScene(labyrinttiNakyma);
+        ikkuna.show();
+    }
+
+    private Scene getLabyrinttiNakyma(Labyrintti labyrintti, Scene luonti, Stage ikkuna) {
+        GridPane ruudukko = new GridPane();
         if (labyrintti != null) {
             for (int i = 0; i < labyrintti.getLeveys(); ++i) {
                 for (int j = 0; j < labyrintti.getKorkeus(); ++j) {
@@ -44,10 +78,21 @@ public class RatkaisijaUi extends Application {
             }
         }
 
-        Scene nakyma = new Scene(ruudukko);
+        Button luo = new Button("Luo uusi labyrintti");
+        Button ratkaise = new Button("Ratkaise labyrintti");
+        VBox labyrinttiNapit = new VBox();
+        labyrinttiNapit.getChildren().add(luo);
+        labyrinttiNapit.getChildren().add(ratkaise);
 
-        ikkuna.setScene(nakyma);
-        ikkuna.show();
+        BorderPane asettelu = new BorderPane();
+        asettelu.setCenter(ruudukko);
+        asettelu.setRight(labyrinttiNapit);
+        Scene labyrinttiNakyma = new Scene(asettelu);
+
+        luo.setOnAction((action) -> {
+            ikkuna.setScene(luonti);
+        });
+        return labyrinttiNakyma;
     }
 
     /**
